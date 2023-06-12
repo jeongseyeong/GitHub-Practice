aac16b753f05332cae48a4e26d1eae6461701626
Esther Jeong <ejeongpuffy@gmail.com>
2023-06-12 08:24:03 -0700

    Initial Commit

---
+++ GitHub Practice/GitHub_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_PracticeApp.swift
+//  GitHub Practice
+//
+//  Created by esther jeong on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
